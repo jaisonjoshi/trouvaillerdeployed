ac2d9490c8a982373641f35dff6738ebdfb70d74
Sheethal31 <75903042+Ella311@users.noreply.github.com>
2022-12-09 20:15:10 +0530

    added search bar

--- admin/src/pages/Hotels/Hotels.js
+++ admin/src/pages/Hotels/Hotels.js
@@ -5,14 +5,48 @@ import './hotels.scss';
 import useFetch from "../../hooks/useFetch"
 import { useEffect, useState } from 'react';
 import HotelCard from '../../components/hotelCard/HotelCard';
+import SearchIcon from '@mui/icons-material/Search'
 
 const Hotels =() => {
-    const [hotels, setHotels] = useState([]);
-    const {data, loading, err} = useFetch("/hotels");
-    useEffect(()=> {
+    //const [hotels, setHotels] = useState([]);
+   // const {data, loading, err} = useFetch("/hotels");
+   {/* useEffect(()=> {
          setHotels(data);
-    }, [data]);
-  
+    }, [data]);*/}
+
+    const [destination,setDestination]=useState("");
+   
+    const url1=`/hotels?location=${destination}`
+    const url2=`/hotels`
+    let url=(destination,url1,url2)=>{
+      
+      if(destination){
+        return url1
+      }
+      else{
+        return url2
+      }
+      
+    }
+    if(destination){
+        url=url1}
+        else
+        url=url2;
+      const {data,loading,error,reFetch}=useFetch(url)
+
+
+
+    const handleClick = () => {
+        reFetch();//handleclick const for all filters,handle chage just sets values
+       }
+       const handleSearchChange = (e) => {
+        //console.log("input val "+e.target.value);
+        let tar=e.target.value;
+        //console.log("IN LOWER CASE "+tar.toLowerCase())
+        //console.log(t.toLowerCase())
+        setDestination(tar.toLowerCase());
+    console.log(destination);
+}
     return(
         <div className="hotels">
             <Navbar />
@@ -21,6 +55,21 @@ const Hotels =() => {
             <div className="hotels-body">
                     <div className="hotels-body-header">
                     <h1>Hotels</h1>
+                    <div className="flex justify-center py-6">
+        <div className="flex items-center w-[30%] justify-between focus:ring-0 focus:ring-offset-0 focus:border-graydust-medium outline-none shadow-sm shadow-gray-500 rounded-2xl text-xs py-2 pl-3">
+          <input
+            type="text border-none outline-none w-[80%] h-[100%] text-2xl"
+            placeholder="Destination"
+            id="destination"
+            name="destination"
+            onChange={handleSearchChange}
+          />
+          <SearchIcon
+            className="w=[20%] mx-3 cursor-pointer"
+            onClick={handleClick}
+          />
+        </div>
+      </div>
                     <Link to="/hotels/newHotel">
                             Create a new Hotel
                         </Link>
@@ -28,7 +77,7 @@ const Hotels =() => {
                     <div className="hotel-container">
                       
                       
-                        { hotels && hotels.map((hotel) => (
+                        { data && data.map((hotel) => (
                             
                             <HotelCard key={hotel._id} hotel={hotel} />
                                 
