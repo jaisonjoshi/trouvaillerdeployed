b54ace4bc879a6b2bdee084b414dd0e47a091700
Irene Rose Johnson <jirenerose@gmail.com>
2023-02-14 23:15:14 +0530

    Added caption for special offers

--- client/src/Pages/home/Home.jsx
+++ client/src/Pages/home/Home.jsx
@@ -89,7 +89,7 @@ Know more
 
       <div className="pt-8 sm:pt-20 pb-8 px-4 sm:px-16 md:px-20 2xl:px-40 ">
         <h1 className='text-center font-bold text-blacky-dark text-2xl sm:text-4xl'>Special Offers</h1>
-                  <p className='text-center textnormal py-4 text-sm  sm:text-base md:text-lg font-body'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore iure odit nemo.</p>
+                  <p className='text-center textnormal py-4 text-sm  sm:text-base md:text-lg font-body'>Get ready to explore the world with our unbeatable travel deals!</p>
       </div>
 
       <div className="px-4 sm:px-16 md:px-20 2xl:px-40 offer-container ">
@@ -105,7 +105,7 @@ Know more
         <div className='pt-4 sm:pt-28 pb-8  px-4 sm:px-16 md:px-20 2xl:px-40'>
           <div className='text-center'>
             <h1 className='font-bold text-2xl sm:text-4xl'>Trending Destinations</h1>
-            <p className=' pt-4 textnormal text-sm md:text-lg'>Have a plan to go vacation? See trending destinations for your inspiration where to go.</p>
+            <p className=' pt-4 textnormal  md:text-lg'>Have a plan to go vacation? See trending destinations for your inspiration where to go.</p>
           </div>
         </div>
         <div className='grid  px-4 sm:px-16 md:px-20 2xl:px-40  sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 auto-rows-[1fr] '>
