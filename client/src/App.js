0a029e9fda93ca7df13894966f97432184a333ec
jaison <jaisonjoshi2001@gmail.com>
2023-05-17 12:44:21 +0530

    baseurl

--- client/src/App.js
+++ client/src/App.js
@@ -77,7 +77,7 @@ function App() {
 </h1>
         </div>
       ) : (
-        <BrowserRouter basename="https://trouvailler.com">
+        <BrowserRouter>
           <ScrollToTop />
           <Routes>
             <Route path="/se" element={<Suspense fallback={<LoadingScreen />}><SearchList location={location} /></Suspense>} />
