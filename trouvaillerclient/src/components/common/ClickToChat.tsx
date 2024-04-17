f0bf4e5f4bb58f910d7bbbe96cd319965bc9a8bf
jaison <jaisonjoshi2001@gmail.com>
2024-04-17 08:27:33 +0530

    ui

--- trouvaillerclient/src/components/common/ClickToChat.tsx
+++ trouvaillerclient/src/components/common/ClickToChat.tsx
@@ -3,10 +3,10 @@ import Link from "next/link"
 
 const ClickToChat = () => {
     return (
-        <Link href="https://wa.me/918129177335 " className=" fixed z-[167890987654456] bg-[#25d366] pl-4 pr-8 py-2 rounded-full animate-bounce overflow-hidden bottom-8 md:bottom-16 right-4 md:right-8 cursor-pointer shadow-custom3">
+        <Link href="https://wa.me/918129177335 " className=" fixed z-[167890987654456] bg-[#25d366] rounded-t-[10px] right-0 transform  py-2 px-4  top-[50%]   cursor-pointer shadow-custom3">
             <div className=" flex items-center justify-center gap-2 ">
                 <img src="images/icons/wagreen.png" className="w-8 md:w-12"/>
-                <h1 className="font-bold text-sm md:text-lg text-[white] ">Chat with Us</h1>
+                <h1 className="font-bold text-sm md:text-lg text-[white]  ">Chat with Us</h1>
             </div>
         </Link>
     )
--- trouvaillerclient/src/components/homePage/Header.tsx
+++ trouvaillerclient/src/components/homePage/Header.tsx
@@ -76,7 +76,7 @@ export const Header: React.FC<{}> = ({ }) => {
         };
       }, []);
     return (
-        <div className="relative pb-16 h-[100vh] md:h-auto">
+        <div className="relative pb-16 h-[100vh] md:h-auto greenbggrad">
              {openSearchComponentMobile && 
             
                 
@@ -84,48 +84,21 @@ export const Header: React.FC<{}> = ({ }) => {
 
              }
 
-            <div className="w-full  absolute top-0 bottom-0 ">
-                <Swiper
-                    spaceBetween={30}
-                    effect={'fade'}
-                    loop
-                    
-                    allowTouchMove={false}
-                    modules={[EffectFade, Autoplay]}
-                    className="mySwiper h-full"
-                >
-                    <SwiperSlide>
-                        <div className="header-bg h-full">
-                            <img src="/images/bg.jpg " className="w-full h-full object-cover" />
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="header-bg">
-                            <img src="/images/slider/slider2.jpg" className="w-full h-full object-cover" />
-                        </div>                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="header-bg">
-                            <img src="/images/slider/slider3.jpg" className="w-full h-full object-cover" />
-                        </div>                    </SwiperSlide>
-
-                </Swiper>
-
-            </div>
+            
            
             <div className="absolute  w-full z-[1000] py-4 sm:py-8">
-                <Navbar />
+                <Navbar darkMode={true}/>
             </div>
-            <div className=" relative text-center z-[20] pt-48 xs:pt-40 lg:pt-48 xl:pt-56 pb-8 xs:pb-20 px-8 sm:px-20 xl:px-60 text-white">
+            <div className=" relative flex items-center gap-10 z-[20] pt-48 xs:pt-40 lg:pt-48 xl:pt-56 pb-8 xs:pb-20 px-8 sm:px-20 xl:px-60 text-black">
                 <div>
-                    <div className="    "> <h1 className="font-bold text-[24px] xs:text-[32px] md:text-[36px] lg:text-[40px] xl:text-[40px] "> Discover Your<br></br><span className="font-extrabold text-[24px] xs:text-[34px] sm:ext-[36px] md:text-[40px] lg:text-[50px] xl:text-[50px] mt-0 xl:mt-2">
+                    <div className="    "> <h1 className="font-medium text-[24px] xs:text-[32px] md:text-[36px] lg:text-[40px] xl:text-[36px] lato "> Discover Your<br></br><span className="font-[600] text-[24px] xs:text-[34px] sm:ext-[36px] md:text-[40px] lg:text-[50px] xl:text-[40px] mt-0 xl:mt-2">
                         Next Adventure <span className="text-[#42b53c]"> With Us</span></span></h1></div>
-                    <p className="text-sm leading-[24px] xs:leading-normal xs:text-base md:text-lg lg:text-xl my-8 xl:my-8    text-[#e4e4e4]">Experience the adventure of a lifetime with our hand-picked travel packages. Book now and create memories that will last a lifetime!</p>
-                </div>
-
-
-
-            </div>
-            <div className="bg-[transparent] hidden  lg:flex justify-between items-center h-[40px] xl:h-[60px] gap-[2%] relative z-10 mx-12 xl:mx-40 2xl:mx-60   ">
+                    <p className="text-sm leading-[24px] xs:leading-normal xs:text-base md:text-lg lg:text-base my-8 xl:my-8    text-[black]">Experience the adventure of a lifetime with our hand-picked travel packages. Book now and create memories that will last a lifetime!</p>
+                    
+                    
+                    
+                    
+                    <div className="bg-[transparent] hidden py-10 px-4 lg:flex justify-between items-center h-[40px] mt-20 xl:h-[60px] gap-[2%] relative z-10 bg-white overflow-hidden rounded shadow-custom-4   ">
                 <div className="w-[32%] flex items-center  bg-[white] px-4 h-full">
                 <div className="flex gap-4 items-center  relative">
                     <img src="/images/icons/loc.svg" alt="" className="w-6" />
@@ -144,31 +117,24 @@ export const Header: React.FC<{}> = ({ }) => {
                     </div>}
                 </div>
                 </div>
-                <div className="w-[32%] bg-[white]  h-full flex items-center px-4">
-                <div className="flex gap-4 items-center relative" >
-                    <img src="/images/icons/grp.svg" alt="" className="w-6" />
-                    <span className="text-[#a9a9a9] text-sm 2xl:text-lg" onClick={() => setShowCategory(!showCategory)}>{category === null ? "Travelling with" : category.charAt(0).toUpperCase()+category.slice(1)}</span>
-                    {showCategory && <div ref={menuRef} className="absolute bottom-0 translate-y-[100%] min-w-[300px] left-14 bg-[white] shadow-xl rounded ">
-                        <ul className="flex flex-col ">
-                            <li className="cursor-pointer hover:bg-[#e5e5e5] px-4 py-1" onClick={() => selectCategory(null)}>Clear selection</li>
-                            <li className="cursor-pointer hover:bg-[#e5e5e5] px-4 py-1" onClick={() => selectCategory("family")}>Family</li>
-                            <li className="cursor-pointer hover:bg-[#e5e5e5] px-4 py-1" onClick={() => selectCategory("honeymoon")}>Honeymoon</li>
-                            <li className="cursor-pointer hover:bg-[#e5e5e5] px-4 py-1" onClick={() => selectCategory("friends")}>Friends</li>
-                            <li className="cursor-pointer hover:bg-[#e5e5e5] px-4 py-1" onClick={() => selectCategory("adventure")}>Adventure</li>
-
-                        </ul>
-
-                    </div>}
-                </div>
-                </div>
-                <div className="w-[32%]">
-                <button className="bg-[#45b773] w-full h-[40px] xl:h-[60px] text-[white]  text-base 2xl:text-xl  font-medium">Search</button>
+                
+                <div className="w-[32%] ">
+                <button className="bg-[#45b773] w-full h-[40px] xl:h-[60px] text-[white] rounded text-base 2xl:text-base  font-bold">Search</button>
 
                 </div>
                
                 
            
             </div>
+                </div>
+                <div>
+                    <img src="/images/travelimg.png" alt="" />
+                </div>
+
+
+
+            </div>
+           
 
             <div>
             <div className="flex lg:hidden  relative z-10 items-center mx-4 sm:mx-20 h-[40px] xs:h-[50px] rounded bg-[white] py-6 pl-2 sm:pl-4">
